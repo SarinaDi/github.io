085e306e8ca050c3ee91a8700f670f5d368749f7
Ninayia <128266759+SarinaDi@users.noreply.github.com>
2026-06-27 00:10:45 +0200

    Update script.js

--- lecteur/script.js
+++ lecteur/script.js
@@ -1,10 +1,10 @@
 const cover = document.querySelector(".cover-screen");
-const reader = document.querySelector(".reader");
+const reader = document.getElementById("reader");
 
-document.getElementById("openBook").onclick = () => {
+document.getElementById("openBook").addEventListener("click", () => {
 
     cover.style.display = "none";
 
     reader.style.display = "flex";
 
-};
+});
