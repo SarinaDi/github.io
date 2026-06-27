e1f7ceb8b8cf65c5f16e18cd8c1cff0762a15a61
Ninayia <128266759+SarinaDi@users.noreply.github.com>
2026-06-27 13:25:45 +0200

    Update livre.js

--- lecteur/livre.js
+++ lecteur/livre.js
@@ -1,1 +1,41 @@
+// =================================================
+// LIVRE
+// =================================================
 
+const pages = [
+
+    {
+
+        leftTitle: "Tome I",
+
+        leftSubtitle: "Le Prince des Enfers",
+
+        rightChapter: "Chapitre I",
+
+        rightTitle: "Une paix fragile",
+
+        rightText:
+`Le royaume des Enfers n'était jamais réellement silencieux.
+
+Même lorsque les flammes semblaient s'endormir, les anciennes pierres murmuraient encore les souvenirs des âmes qui avaient traversé leurs portes.`
+
+    },
+
+    {
+
+        leftTitle: "Tome I",
+
+        leftSubtitle: "Le Prince des Enfers",
+
+        rightChapter: "Chapitre II",
+
+        rightTitle: "Le Prince",
+
+        rightText:
+`Kaël observait les flammes éternelles depuis le balcon du palais.
+
+Depuis toujours, ce royaume était le sien.`
+
+    }
+
+];
