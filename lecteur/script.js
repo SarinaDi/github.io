5c8185e78aeff4aa1087ababab04dacff2345a55
Ninayia <128266759+SarinaDi@users.noreply.github.com>
2026-06-27 09:03:45 +0200

    Update script.js

--- lecteur/script.js
+++ lecteur/script.js
@@ -1,10 +1,8 @@
-const cover = document.getElementById("coverScreen");
-const reader = document.getElementById("reader");
+const book = document.getElementById("book");
 const button = document.getElementById("openBook");
 
 button.addEventListener("click", () => {
 
-    cover.style.display = "none";
-    reader.style.display = "flex";
+    book.classList.add("open");
 
 });
