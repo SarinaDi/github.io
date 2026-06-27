36ed428b14670fa1d4693e293abfb660be3e9b88
Ninayia <128266759+SarinaDi@users.noreply.github.com>
2026-06-27 09:07:59 +0200

    Update script.js

--- lecteur/script.js
+++ lecteur/script.js
@@ -3,6 +3,17 @@ const button = document.getElementById("openBook");
 
 button.addEventListener("click", () => {
 
+    // Lance l'animation
     book.classList.add("open");
+
+    // Désactive le bouton
+    button.disabled = true;
+
+    // Attend la fin de l'animation
+    setTimeout(() => {
+
+        window.location.href = "lecture/index.html";
+
+    }, 1200);
 
 });
